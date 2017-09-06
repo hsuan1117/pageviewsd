69787dc0237bf194390ec2f13c8b216ea22b2c9b
nechehin <nechehin@gmail.com>
2017-09-06 18:03:07 +0300

    Remove x-powered-by response header

--- app/app.js
+++ app/app.js
@@ -4,6 +4,9 @@ const config  = require('./config.json')
 const worker  = require('./worker')
 const debug   = require('debug')('http')
 
+// Remove x-powered-by response header
+app.disable('x-powered-by');
+
 // Register routes
 app.use('/hit',   require('./routes/hit'));
 app.use('/get',   require('./routes/get'));
