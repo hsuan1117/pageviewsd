a96c7b461c7e547df23cfa015cc96711bd2b7ca4
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-08-09 15:08:10 +0800

    new format

--- app/routes/get.js
+++ app/routes/get.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const counter = require('./../counter');
-const config  = require('./../config');
-const router  = express.Router();
+const config = require('./../config');
+const router = express.Router();
 
 
 function requestHandler(req, res) {
@@ -9,17 +9,21 @@ function requestHandler(req, res) {
     res.setHeader('Content-Type', 'application/json');
 
     if (!~config.labels.indexOf(req.params.label)) {
-        res.status(403).json({ error: 'Label not allowed' });
+        res.status(403).json({error: 'Label not allowed'});
         return;
     }
 
-    counter.getKeyScores(req.params.label + '-merged', function(ids){
-        res.json(Object.keys(ids).map(x=>ids[x]));
+    counter.getKeyScores(req.params.label + '-merged', function (ids) {
+        if (req.params.id) {
+            res.json(Object.keys(ids).filter(x => ids[x].id === req.params.id).map(x => ids[x]));
+        } else {
+            res.json(Object.keys(ids).map(x => ids[x]));
+        }
     });
 }
 
 
-router.get('/:label/:days(\\d+)', requestHandler);
+router.get('/:label/:id', requestHandler);
 router.get('/:label', requestHandler);
 
 module.exports = router;
