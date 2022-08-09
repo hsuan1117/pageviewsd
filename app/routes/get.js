0deae15510b0b670224bc9f3ab86667c584044c4
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-08-09 15:08:52 +0800

    new format

--- app/routes/get.js
+++ app/routes/get.js
@@ -15,7 +15,7 @@ function requestHandler(req, res) {
 
     counter.getKeyScores(req.params.label + '-merged', function (ids) {
         if (req.params.id) {
-            res.json(Object.keys(ids).filter(x => ids[x].id === req.params.id).map(x => ids[x]));
+            res.json(Object.keys(ids).filter(x => ids[x].id === req.params.id).map(x => ids[x])[0]);
         } else {
             res.json(Object.keys(ids).map(x => ids[x]));
         }
